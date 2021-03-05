4474cafd84378a29cf9feb85377e777efc357e8e
xFueY <62284893+xFueY@users.noreply.github.com>
2021-03-05 10:11:07 +0100

    Updated a bit

--- bot.py
+++ bot.py
@@ -9,11 +9,6 @@ from time import strftime
 config = json.load(open("config.json"))
 
 AuthGGColor = 0x3569E6
-AuthGGName = "Auth.GG"
-AuthGGIcon = "https://i.imgur.com/6xMaB6h.png"
-AuthGGUrl = "https://auth.gg/"
-
-AuthGGBotVersion = "1.0.0"
 
 client = commands.Bot(command_prefix=str(config['CommandPrefix']), case_insensitive=True)
 client.remove_command("help")
@@ -21,6 +16,7 @@ client.remove_command("help")
 AuthAdmin = PyAuthGG.Administration(str(config['AuthGGAdminAPI']))
 AuthApp = PyAuthGG.Application(str(config['AuthGGAPI']), str(config['AuthGGAID']), str(config['AuthGGSecret']))
 
+AuthGGBotVersion = "1.0.0"
 
 @client.event
 async def on_command_error(ctx, error):
@@ -31,6 +27,8 @@ async def on_command_error(ctx, error):
     elif isinstance(error, commands.BadArgument):
         SendError = True
     elif isinstance(error, commands.MissingPermissions):
+        SendError = True
+    elif isinstance(error, commands.CommandNotFound):
         SendError = True
 
     if SendError == True:
@@ -46,7 +44,7 @@ async def on_command_error(ctx, error):
 async def Help(ctx, Category="None"):
     HelpEmbed=discord.Embed(title="AuthGG Help", description=f"Here you can find a list of all available commands, and how to use them.\n``<Argument>`` = Required\n``[Argument | Default]`` = Not Required", color=AuthGGColor, timestamp=datetime.datetime.utcnow())
     HelpEmbed.set_thumbnail(url="https://i.imgur.com/3GxXzyn.png")
-    HelpEmbed.set_author(name=AuthGGName, icon_url=AuthGGIcon, url=AuthGGUrl)
+    HelpEmbed.set_author(name=config['AuthGGName'], icon_url=config['AuthGGIcon'], url=config['AuthGGUrl'])
     HelpEmbed.set_image(url="https://i.imgur.com/JT7PECu.png")
     HelpEmbed.set_footer(text="Developed by xFueY#7575")
 
@@ -69,7 +67,7 @@ async def Help(ctx, Category="None"):
 async def Info(ctx):
     CreditsEmbed=discord.Embed(title="AuthGG Discord Bot", color=AuthGGColor, timestamp=datetime.datetime.utcnow())
     CreditsEmbed.set_thumbnail(url="https://i.imgur.com/cd3jiBe.png")
-    CreditsEmbed.set_author(name=AuthGGName, icon_url=AuthGGIcon, url=AuthGGUrl)
+    CreditsEmbed.set_author(name=config['AuthGGName'], icon_url=config['AuthGGIcon'], url=config['AuthGGUrl'])
     CreditsEmbed.set_image(url="https://i.imgur.com/JT7PECu.png")
     CreditsEmbed.set_footer(text=ctx.guild.name, icon_url=ctx.guild.icon_url_as(format=None, static_format='png', size=1024))
 
@@ -88,7 +86,7 @@ async def AppInfo(ctx):
     FetchedInfo = AuthApp.Info()
 
     AppStatsEmbed=discord.Embed(title=FetchedInfo['name'], description=f"**Status:** {FetchedInfo['status']}\n**Version:** {FetchedInfo['version']}\n**Users:** {FetchedInfo['users']}\n**Licenses:** {FetchedLicenseCount['value']}\n\n**Login:** {FetchedInfo['login']}\n**Register:** {FetchedInfo['register']}\n**Freemode:** {FetchedInfo['freemode']}\n**Developermode:** {FetchedInfo['developermode']}", color=AuthGGColor, timestamp=datetime.datetime.utcnow())
-    AppStatsEmbed.set_author(name=AuthGGName, icon_url=AuthGGIcon, url=AuthGGUrl)
+    AppStatsEmbed.set_author(name=config['AuthGGName'], icon_url=config['AuthGGIcon'], url=config['AuthGGUrl'])
     AppStatsEmbed.set_thumbnail(url=config['AppInfoLogo'])
     AppStatsEmbed.set_footer(text=ctx.guild.name, icon_url=ctx.guild.icon_url_as(format=None, static_format='png', size=1024))
     await ctx.send(embed=AppStatsEmbed)
@@ -100,8 +98,8 @@ async def AuthStatus(ctx):
     FetchedStatus = AuthAdmin.Status()
 
     AuthStatusEmbed=discord.Embed(title="AuthGG Status", color=AuthGGColor, timestamp=datetime.datetime.utcnow())
-    AuthStatusEmbed.set_author(name=AuthGGName, icon_url=AuthGGIcon, url=AuthGGUrl)
-    AuthStatusEmbed.set_thumbnail(url=AuthGGIcon)
+    AuthStatusEmbed.set_author(name=config['AuthGGName'], icon_url=config['AuthGGIcon'], url=config['AuthGGUrl'])
+    AuthStatusEmbed.set_thumbnail(url=config['AuthGGIcon'])
     AuthStatusEmbed.set_footer(text=ctx.guild.name, icon_url=ctx.guild.icon_url_as(format=None, static_format='png', size=1024))
 
     AuthStatusEmbed.add_field(name="Backend/API", value=str(FetchedStatus['Backend/API']), inline=False)
@@ -128,7 +126,7 @@ async def FetchUser(ctx, Username):
 
     else:
         FetchUserEmbed=discord.Embed(title=Username, description=f"**Status:** {FetchedUser['status'].replace('failed', 'Failed')}\n**Message:** {FetchedUser['info']}", color=AuthGGColor, timestamp=datetime.datetime.utcnow())
-        FetchUserEmbed.set_author(name=AuthGGName, icon_url=AuthGGIcon, url=AuthGGUrl)
+        FetchUserEmbed.set_author(name=config['AuthGGName'], icon_url=config['AuthGGIcon'], url=config['AuthGGUrl'])
         FetchUserEmbed.set_footer(text=ctx.guild.name, icon_url=ctx.guild.icon_url_as(format=None, static_format='png', size=1024))
         await ctx.send(embed=FetchUserEmbed)
 
@@ -141,7 +139,7 @@ async def FetchUsers(ctx):
     CurrentNumber = 0
 
     FetchedUsersEmbed=discord.Embed(title=str(FetchedUsers[str(CurrentNumber)]['username']) + " (" + str(CurrentNumber + 1) + "/" + str(len(FetchedUsers)) + ")", description=f"**Username:** {FetchedUsers[str(CurrentNumber)]['username']}\n**E-Mail:** {FetchedUsers[str(CurrentNumber)]['email']}\n**Rank:** {FetchedUsers[str(CurrentNumber)]['rank']}\n**HWID:** {FetchedUsers[str(CurrentNumber)]['hwid']}\n**Variable:** {FetchedUsers[str(CurrentNumber)]['variable']}\n**Last Login:** {FetchedUsers[str(CurrentNumber)]['lastlogin']}\n**Last IP:** {FetchedUsers[str(CurrentNumber)]['lastip']}\n**Expiry:** {FetchedUsers[str(CurrentNumber)]['expiry_date']}", color=AuthGGColor, timestamp=datetime.datetime.utcnow())
-    FetchedUsersEmbed.set_author(name=AuthGGName, icon_url=AuthGGIcon, url=AuthGGUrl)
+    FetchedUsersEmbed.set_author(name=config['AuthGGName'], icon_url=config['AuthGGIcon'], url=config['AuthGGUrl'])
     FetchedUsersEmbed.set_footer(text=ctx.guild.name, icon_url=ctx.guild.icon_url_as(format=None, static_format='png', size=1024))
     MainMessage = await ctx.send(embed=FetchedUsersEmbed)
 
@@ -178,7 +176,7 @@ async def FetchUsers(ctx):
                 CurrentNumber = len(FetchedUsers) - 1
 
             NewFetchedUsersEmbed=discord.Embed(title=str(FetchedUsers[str(CurrentNumber)]['username']) + " (" + str(CurrentNumber + 1) + "/" + str(len(FetchedUsers)) + ")", description=f"**Username:** {FetchedUsers[str(CurrentNumber)]['username']}\n**E-Mail:** {FetchedUsers[str(CurrentNumber)]['email']}\n**Rank:** {FetchedUsers[str(CurrentNumber)]['rank']}\n**HWID:** {FetchedUsers[str(CurrentNumber)]['hwid']}\n**Variable:** {FetchedUsers[str(CurrentNumber)]['variable']}\n**Last Login:** {FetchedUsers[str(CurrentNumber)]['lastlogin']}\n**Last IP:** {FetchedUsers[str(CurrentNumber)]['lastip']}\n**Expiry:** {FetchedUsers[str(CurrentNumber)]['expiry_date']}", color=AuthGGColor, timestamp=datetime.datetime.utcnow())
-            NewFetchedUsersEmbed.set_author(name=AuthGGName, icon_url=AuthGGIcon, url=AuthGGUrl)
+            NewFetchedUsersEmbed.set_author(name=config['AuthGGName'], icon_url=config['AuthGGIcon'], url=config['AuthGGUrl'])
             NewFetchedUsersEmbed.set_footer(text=ctx.guild.name, icon_url=ctx.guild.icon_url_as(format=None, static_format='png', size=1024))
             await MainMessage.edit(embed=NewFetchedUsersEmbed)
 
@@ -188,8 +186,8 @@ async def FetchUsers(ctx):
 async def DeleteUser(ctx, Username):
     DeletedUser = AuthAdmin.DeleteUser(Username)
 
-    DeleteUserEmbed=discord.Embed(title="Delete User", description=f"**Status:** {DeletedUser['value']}\n**Info:** {DeletedUser['info']}", color=AuthGGColor, timestamp=datetime.datetime.utcnow())
-    DeleteUserEmbed.set_author(name=AuthGGName, icon_url=AuthGGIcon, url=AuthGGUrl)
+    DeleteUserEmbed=discord.Embed(title="Delete User", description=f"**Status:** {DeletedUser['status']}\n**Info:** {DeletedUser['info']}", color=AuthGGColor, timestamp=datetime.datetime.utcnow())
+    DeleteUserEmbed.set_author(name=config['AuthGGName'], icon_url=config['AuthGGIcon'], url=config['AuthGGUrl'])
     DeleteUserEmbed.set_footer(text=ctx.guild.name, icon_url=ctx.guild.icon_url_as(format=None, static_format='png', size=1024))
     await ctx.send(embed=DeleteUserEmbed)
 
@@ -200,7 +198,7 @@ async def ChangeVariable(ctx, Username, *, Variable):
     ChangedVariable = AuthAdmin.ChangeVariable(Username, Variable)
 
     ChangeVariableEmbed=discord.Embed(title="Change Variable", description=f"**Status:** {ChangedVariable['status']}\n**Info:** {ChangedVariable['info']}", color=AuthGGColor, timestamp=datetime.datetime.utcnow())
-    ChangeVariableEmbed.set_author(name=AuthGGName, icon_url=AuthGGIcon, url=AuthGGUrl)
+    ChangeVariableEmbed.set_author(name=config['AuthGGName'], icon_url=config['AuthGGIcon'], url=config['AuthGGUrl'])
     ChangeVariableEmbed.set_footer(text=ctx.guild.name, icon_url=ctx.guild.icon_url_as(format=None, static_format='png', size=1024))
     await ctx.send(embed=ChangeVariableEmbed)
 
@@ -211,7 +209,7 @@ async def ChangeRank(ctx, Username, Rank : int):
     ChangedRank = AuthAdmin.ChangeRank(Username, Rank)
 
     ChangeRankEmbed=discord.Embed(title="Change Variable", description=f"**Status:** {ChangedRank['status']}\n**Info:** {ChangedRank['info']}", color=AuthGGColor, timestamp=datetime.datetime.utcnow())
-    ChangeRankEmbed.set_author(name=AuthGGName, icon_url=AuthGGIcon, url=AuthGGUrl)
+    ChangeRankEmbed.set_author(name=config['AuthGGName'], icon_url=config['AuthGGIcon'], url=config['AuthGGUrl'])
     ChangeRankEmbed.set_footer(text=ctx.guild.name, icon_url=ctx.guild.icon_url_as(format=None, static_format='png', size=1024))
     await ctx.send(embed=ChangeRankEmbed)
 
@@ -222,7 +220,7 @@ async def ChangePassword(ctx, Username, Password):
     ChangedPassword = AuthAdmin.ChangePassword(Username, Password)
 
     ChangePasswordEmbed=discord.Embed(title="Change Password", description=f"**Status:** {ChangedPassword['status']}\n**Info:** {ChangedPassword['info']}", color=AuthGGColor, timestamp=datetime.datetime.utcnow())
-    ChangePasswordEmbed.set_author(name=AuthGGName, icon_url=AuthGGIcon, url=AuthGGUrl)
+    ChangePasswordEmbed.set_author(name=config['AuthGGName'], icon_url=config['AuthGGIcon'], url=config['AuthGGUrl'])
     ChangePasswordEmbed.set_footer(text=ctx.guild.name, icon_url=ctx.guild.icon_url_as(format=None, static_format='png', size=1024))
     await ctx.send(embed=ChangePasswordEmbed)
 
@@ -234,13 +232,13 @@ async def FetchLicense(ctx, License):
 
     if FetchedLicense['status'] == "success":
         FetchLicenseEmbed=discord.Embed(title="Fetch License", description=f"**Status:** {FetchedLicense['status']}\n**License:** {FetchedLicense['license']}\n**Rank:** {FetchedLicense['rank']}\n**Used:** {FetchedLicense['used']}\n**Used By:** {FetchedLicense['used_by']}\n**Created:** {FetchedLicense['created']}", color=AuthGGColor, timestamp=datetime.datetime.utcnow())
-        FetchLicenseEmbed.set_author(name=AuthGGName, icon_url=AuthGGIcon, url=AuthGGUrl)
+        FetchLicenseEmbed.set_author(name=config['AuthGGName'], icon_url=config['AuthGGIcon'], url=config['AuthGGUrl'])
         FetchLicenseEmbed.set_footer(text=ctx.guild.name, icon_url=ctx.guild.icon_url_as(format=None, static_format='png', size=1024))
         await ctx.send(embed=FetchLicenseEmbed)
 
     else:
         FetchLicenseEmbed=discord.Embed(title="Fetch License", description=f"**Status:** {FetchedLicense['status']}\n**Info:** {FetchedLicense['info']}", color=AuthGGColor, timestamp=datetime.datetime.utcnow())
-        FetchLicenseEmbed.set_author(name=AuthGGName, icon_url=AuthGGIcon, url=AuthGGUrl)
+        FetchLicenseEmbed.set_author(name=config['AuthGGName'], icon_url=config['AuthGGIcon'], url=config['AuthGGUrl'])
         FetchLicenseEmbed.set_footer(text=ctx.guild.name, icon_url=ctx.guild.icon_url_as(format=None, static_format='png', size=1024))
         await ctx.send(embed=FetchLicenseEmbed)
 
@@ -253,7 +251,7 @@ async def FetchLicenses(ctx):
     CurrentNumber = 0
 
     FetchLicensesEmbed=discord.Embed(title="Fetch Licenses (" + str(CurrentNumber + 1) + "/" + str(len(FetchedLicenses)) + ")", description=f"**License:** {FetchedLicenses[str(CurrentNumber)]['token']}\n**Rank:** {FetchedLicenses[str(CurrentNumber)]['rank']}\n**Rank:** {FetchedLicenses[str(CurrentNumber)]['rank']}\n**Used:** {FetchedLicenses[str(CurrentNumber)]['used'].replace('0', 'False').replace('1', 'True')}\n**Used By:** {FetchedLicenses[str(CurrentNumber)]['used_by']}\n**Days:** {FetchedLicenses[str(CurrentNumber)]['days']}", color=AuthGGColor, timestamp=datetime.datetime.utcnow())
-    FetchLicensesEmbed.set_author(name=AuthGGName, icon_url=AuthGGIcon, url=AuthGGUrl)
+    FetchLicensesEmbed.set_author(name=config['AuthGGName'], icon_url=config['AuthGGIcon'], url=config['AuthGGUrl'])
     FetchLicensesEmbed.set_footer(text=ctx.guild.name, icon_url=ctx.guild.icon_url_as(format=None, static_format='png', size=1024))
     MainMessage = await ctx.send(embed=FetchLicensesEmbed)
 
@@ -290,7 +288,7 @@ async def FetchLicenses(ctx):
                 CurrentNumber = len(FetchedLicenses) - 1
 
             NewFetchLicensesEmbed=discord.Embed(title="Fetch Licenses (" + str(CurrentNumber + 1) + "/" + str(len(FetchedLicenses)) + ")", description=f"**License:** {FetchedLicenses[str(CurrentNumber)]['token']}\n**Rank:** {FetchedLicenses[str(CurrentNumber)]['rank']}\n**Rank:** {FetchedLicenses[str(CurrentNumber)]['rank']}\n**Used:** {FetchedLicenses[str(CurrentNumber)]['used'].replace('0', 'False').replace('1', 'True')}\n**Used By:** {FetchedLicenses[str(CurrentNumber)]['used_by']}\n**Days:** {FetchedLicenses[str(CurrentNumber)]['days']}", color=AuthGGColor, timestamp=datetime.datetime.utcnow())
-            NewFetchLicensesEmbed.set_author(name=AuthGGName, icon_url=AuthGGIcon, url=AuthGGUrl)
+            NewFetchLicensesEmbed.set_author(name=config['AuthGGName'], icon_url=config['AuthGGIcon'], url=config['AuthGGUrl'])
             NewFetchLicensesEmbed.set_footer(text=ctx.guild.name, icon_url=ctx.guild.icon_url_as(format=None, static_format='png', size=1024))
             await MainMessage.edit(embed=NewFetchLicensesEmbed)
 
@@ -301,7 +299,7 @@ async def DeleteLicense(ctx, License):
     DeletedLicense = AuthAdmin.DeleteLicense(License)
 
     DeleteLicenseEmbed=discord.Embed(title="Delete License", description=f"**Status:** {DeletedLicense['status']}\n**Info:** {DeletedLicense['info']}", color=AuthGGColor, timestamp=datetime.datetime.utcnow())
-    DeleteLicenseEmbed.set_author(name=AuthGGName, icon_url=AuthGGIcon, url=AuthGGUrl)
+    DeleteLicenseEmbed.set_author(name=config['AuthGGName'], icon_url=config['AuthGGIcon'], url=config['AuthGGUrl'])
     DeleteLicenseEmbed.set_footer(text=ctx.guild.name, icon_url=ctx.guild.icon_url_as(format=None, static_format='png', size=1024))
     await ctx.send(embed=DeleteLicenseEmbed)
 
@@ -312,7 +310,7 @@ async def UseLicense(ctx, License):
     UsedLicense = AuthAdmin.UseLicense(License)
 
     UseLicenseEmbed=discord.Embed(title="Use License", description=f"**Status:** {UsedLicense['status']}\n**Info:** {UsedLicense['info']}", color=AuthGGColor, timestamp=datetime.datetime.utcnow())
-    UseLicenseEmbed.set_author(name=AuthGGName, icon_url=AuthGGIcon, url=AuthGGUrl)
+    UseLicenseEmbed.set_author(name=config['AuthGGName'], icon_url=config['AuthGGIcon'], url=config['AuthGGUrl'])
     UseLicenseEmbed.set_footer(text=ctx.guild.name, icon_url=ctx.guild.icon_url_as(format=None, static_format='png', size=1024))
     await ctx.send(embed=UseLicenseEmbed)
 
@@ -323,7 +321,7 @@ async def UnuseLicense(ctx, License):
     UnusedLicense = AuthAdmin.UnuseLicense(License)
 
     UnuseLicenseEmbed=discord.Embed(title="Use License", description=f"**Status:** {UnusedLicense['status']}\n**Info:** {UnusedLicense['info']}", color=AuthGGColor, timestamp=datetime.datetime.utcnow())
-    UnuseLicenseEmbed.set_author(name=AuthGGName, icon_url=AuthGGIcon, url=AuthGGUrl)
+    UnuseLicenseEmbed.set_author(name=config['AuthGGName'], icon_url=config['AuthGGIcon'], url=config['AuthGGUrl'])
     UnuseLicenseEmbed.set_footer(text=ctx.guild.name, icon_url=ctx.guild.icon_url_as(format=None, static_format='png', size=1024))
     await ctx.send(embed=UnuseLicenseEmbed)
 
@@ -338,7 +336,7 @@ async def GenerateLicense(ctx, Amount : int = 1, Days : int = 1, Level : int = 1
         LicenseListed = "".join(x[1] + "\n" for x in GeneratedLicense.items()) # str(int(x[0]) + 1) + " | " +
 
         GenerateLicenseEmbed=discord.Embed(title="Generate License", description=f"```\n{LicenseListed}```", color=AuthGGColor, timestamp=datetime.datetime.utcnow())
-        GenerateLicenseEmbed.set_author(name=AuthGGName, icon_url=AuthGGIcon, url=AuthGGUrl)
+        GenerateLicenseEmbed.set_author(name=config['AuthGGName'], icon_url=config['AuthGGIcon'], url=config['AuthGGUrl'])
         GenerateLicenseEmbed.set_footer(text=ctx.guild.name, icon_url=ctx.guild.icon_url_as(format=None, static_format='png', size=1024))
         await ctx.send(embed=GenerateLicenseEmbed)
 
@@ -354,13 +352,13 @@ async def FetchHWID(ctx, Username):
 
     if FetchedHWID['status'] == "success":
         FetchHWIDEmbed=discord.Embed(title="Fetch HWID", description=f"**Status:** {FetchedHWID['status']}\n**HWID:** {FetchedHWID['value']}", color=AuthGGColor, timestamp=datetime.datetime.utcnow())
-        FetchHWIDEmbed.set_author(name=AuthGGName, icon_url=AuthGGIcon, url=AuthGGUrl)
+        FetchHWIDEmbed.set_author(name=config['AuthGGName'], icon_url=config['AuthGGIcon'], url=config['AuthGGUrl'])
         FetchHWIDEmbed.set_footer(text=ctx.guild.name, icon_url=ctx.guild.icon_url_as(format=None, static_format='png', size=1024))
         await ctx.send(embed=FetchHWIDEmbed)
 
     else:
         FetchHWIDEmbed=discord.Embed(title="Fetch HWID", description=f"**Status:** {FetchedHWID['status']}\n**Info:** {FetchedHWID['info']}", color=AuthGGColor, timestamp=datetime.datetime.utcnow())
-        FetchHWIDEmbed.set_author(name=AuthGGName, icon_url=AuthGGIcon, url=AuthGGUrl)
+        FetchHWIDEmbed.set_author(name=config['AuthGGName'], icon_url=config['AuthGGIcon'], url=config['AuthGGUrl'])
         FetchHWIDEmbed.set_footer(text=ctx.guild.name, icon_url=ctx.guild.icon_url_as(format=None, static_format='png', size=1024))
         await ctx.send(embed=FetchHWIDEmbed)
 
@@ -371,7 +369,7 @@ async def ResetHWID(ctx, Username):
     ResetHWIDResponse = AuthAdmin.ResetHWID(Username)
 
     ResetHWIDEmbed=discord.Embed(title="Reset HWID", description=f"**Status:** {ResetHWIDResponse['status']}\n**Info:** {ResetHWIDResponse['info']}", color=AuthGGColor, timestamp=datetime.datetime.utcnow())
-    ResetHWIDEmbed.set_author(name=AuthGGName, icon_url=AuthGGIcon, url=AuthGGUrl)
+    ResetHWIDEmbed.set_author(name=config['AuthGGName'], icon_url=config['AuthGGIcon'], url=config['AuthGGUrl'])
     ResetHWIDEmbed.set_footer(text=ctx.guild.name, icon_url=ctx.guild.icon_url_as(format=None, static_format='png', size=1024))
     await ctx.send(embed=ResetHWIDEmbed)
 
@@ -382,7 +380,7 @@ async def SetHWID(ctx, Username, HWID):
     SetHWIDResponse = AuthAdmin.SetHWID(Username, HWID)
 
     SetHWIDEmbed=discord.Embed(title="Set HWID", description=f"**Status:** {SetHWIDResponse['status']}\n**Info:** {SetHWIDResponse['info']}", color=AuthGGColor, timestamp=datetime.datetime.utcnow())
-    SetHWIDEmbed.set_author(name=AuthGGName, icon_url=AuthGGIcon, url=AuthGGUrl)
+    SetHWIDEmbed.set_author(name=config['AuthGGName'], icon_url=config['AuthGGIcon'], url=config['AuthGGUrl'])
     SetHWIDEmbed.set_footer(text=ctx.guild.name, icon_url=ctx.guild.icon_url_as(format=None, static_format='png', size=1024))
     await ctx.send(embed=SetHWIDEmbed)
 
